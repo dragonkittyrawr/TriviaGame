06d6673ecc789554b14e5ff78415109d3fc951ec
Erin Stewart <dragonkittyrawr@users.noreply.github.com>
2017-03-24 16:37:28 -0500

    Start of image functionality.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Question Key
 // var questions = {
 //     q1: [
 //         "correct",
@@ -9,20 +10,24 @@
 // };
 
 
+// Question/Answer Bank
+
 var questions = {
     q1: [
         "Nibbler",
         "Norman",
         "Nubben",
         "Gnawer",
-        "What is the name of Leela's Pet?"
+        "What is the name of Leela's Pet?",
+        "assets/images/leelaMeetsNibbler.jpg"
     ],
     q2: [
         "Pizza Delivery Boy",
         "Pool Boy",
         "Taxi Driver",
         "Bicycle Messenger",
-        "What was Fry's job in 1999?"
+        "What was Fry's job in 1999?",
+        "assets/images/panucciPizza.png"
     ],
 
     q3: [
@@ -30,14 +35,16 @@ var questions = {
         "Intersteller Express",
         "Planet Delivery",
         "Space Delivery",
-        "What company does Dr. Farnsworth run?"
+        "What company does Dr. Farnsworth run?",
+        "assets/images/planetExpress.jpg"
     ],
     q4: [
         "Kif Kroker",
         "Al Roker",
         "Cif Coker",
         "Yik Kroker",
-        "What is the name of Zapp Brannigan's lieutenant?"
+        "What is the name of Zapp Brannigan's lieutenant?",
+        "assets/images/kifKroker.gif"
     ],
 
     q5: [
@@ -45,7 +52,8 @@ var questions = {
         "U.S.A",
         "Germany",
         "China",
-        "Bender was manufactured in which country?"
+        "Bender was manufactured in which country?",
+        "assets/images/benderHechoEnMexico.jpg"
     ],
 
     q6: [
@@ -53,15 +61,17 @@ var questions = {
         "Leela",
         "Munda",
         "Smizmar",
-        "What is Leela's first name?"
+        "What is Leela's first name?",
+        "assets/images/turangaLeela.png"
     ],
 
     q7: [
-        "A Hyper Intelligent Ape Named Gunther",
+        "A Hyper intelligent ape named Gunther",
         "A Neptonian",
         "Himself from another dimension",
         "Flexo",
-        "When Fry returns to college, who does he room with?"
+        "When Fry returns to college, who does he room with?",
+        "assets/images/guntherAndFry.jpg"
     ],
 
     q8: [
@@ -69,7 +79,8 @@ var questions = {
         "To win a bet with Hermes.",
         "They thought they were entering a raffle.",
         "They were hypnotized.",
-        "Why did Fry and Bender enlist in the Earth Army?"
+        "Why did Fry and Bender enlist in the Earth Army?",
+        "assets/images/hamGumAllBones.jpg"
     ],
 
     q9: [
@@ -77,7 +88,8 @@ var questions = {
         "Chump",
         "Chumpette",
         "Pimpmobile",
-        "Which word is revealed to be Bender's most commonly used?"
+        "Which word is revealed to be Bender's second most commonly used?",
+        "assets/images/daffodil.jpg"
     ],
 
     q10: [
@@ -85,65 +97,96 @@ var questions = {
         "Professor Farnsworth",
         "Scruffy, the janior",
         "Hermes",
-        "When Fry's head is detached in \"Put Your Head on My Shoulders\", whose shoulder is it attached to?"
+        "When Fry's head is detached in \"Put Your Head on My Shoulders\", whose shoulder is it attached to?",
+        "assets/images/headOnYourShoulders.jpg"
     ]
 };
+
+// Capture length of questions object.
 
 var objSize = Object.keys(questions).length;
 
 
-//  30 second timer.
+//  15 second timer.
 var tickTock = 15;
 
-//  Variable that will hold our interval ID when we execute the "run" function
+//  intervalID is needed for run function.
 var intervalId;
+
+// num is needed to move through the questions object.
 
 var num = 1;
 
+// Score represents correctly answers questions.
+
 var score = 0;
 
+// Capture user selection.
 
 $(".answer").click(function answerSelect() {
-    // console.log($(this).attr("value"));
-    // console.log($(this));
-    // console.log(questions.question1[0].ak);
-    // console.log($(this).text());
 
     //  When an answer is selected stop the timer.
 
     stop();
 
-    console.log(objSize);
+    $("#pictureShow").html("<img src=\"" + questions["q" + num][5] + "\"></img>")
 
-    console.log(questions);
+    // If this is not the last question.
 
     if (num !== objSize) {
 
-        //  When the answer is found correct or incorrect, restart timer.
+        // If the user selected the correct answer.
 
         if ($(this).text() === questions["q" + num][0]) {
 
+            // Display "Correct!".
             $("#answerShow").html("<h2>Correct!</h2>");
+
+            // Increase num to move to next question.
             num++;
+
+            // Increase score.
             score++;
+
+            // Hide answers.
             $("#gameShow").hide();
+
+            // Update correct answers display.
             $("#scoreShow").html("<h2>Correct Answers: " + score + "</h2>");
+
+            // Delay next question for 1.5 seconds.
             setTimeout(function() {
+                //  Restart timer.
                 run();
             }, 1500);
         } else {
+
+            // Display correct answer.
             $("#answerShow").html("<h2>Sorry! The correct answer was \"" + questions["q" + num][0] + "\"</h2>");
+
+            // Increase num to move to next question.
             num++;
+
+            // Hide answers.
             $("#gameShow").hide();
+
+            // Delay next question for 1.5 seconds.
             setTimeout(function() {
+                // Restart timer.
                 run();
             }, 1500);
         }
 
-    } else if (num === objSize) {
+        // If this is the last question.
+    } else if (num === objSize || tickTock === 0) {
+        
+        // Stop the timer.
         stop();
-        score++;
+
+        // Display game over screen.
         $("#gameShow").html("<h2>Game Over!  You got " + score + " correct!</h2>");
+
+        // Clear out scoreShow, questionShow, answerShow, and timerShow
         $("#scoreShow").html("");
         $("#questionShow").html("");
         $("#answerShow").html("");
@@ -152,51 +195,30 @@ $(".answer").click(function answerSelect() {
 
 })
 
-
-
-
-
-// Getting the first row of the table
-var firstRowTd = $("table").children().eq(0).children("tr").eq(0).children("td");
-
-// Getting the second row of the table
-var secondRowTd = $("table").children().eq(0).children("tr").eq(1).children("td");
-
-// Getting the third row of the table
-var thirdRowTd = $("table").children().eq(0).children("tr").eq(2).children("td");
-
-// Getting the fourth row of the table
-var fourthRowTd = $("table").children().eq(0).children("tr").eq(3).children("td");
-
+// Empty array to randomize answer placement.
 var rando = [];
 
-// // Setting the inner text of each td in the first row
-// firstRowTd.eq(0).html(questions.q1[a1]);
-
-// // Setting the inner text of each td in the first row
-// secondRowTd.eq(0).html(questions.q1[a2]);
-
-// // Setting the inner text of each td in the first row
-// thirdRowTd.eq(0).html(questions.q1[a3]);
-
-// // Setting the inner text of each td in the first row
-// fourthRowTd.eq(0).html(questions.q1[a4]);
-
-
-//  Timer runs for 30 seconds.
+//  Main game functionality.
 function run() {
+    // Establish one second interval for timer.
     intervalId = setInterval(decrement, 1000);
 
+    // Blank out answerShow.
     $("#answerShow").html("");
+    $("#pictureShow").html("");
 
+    // Unhide gameShow.
     $("#gameShow").show();
 
-    // http://stackoverflow.com/questions/962802#962890
+    // Answer sorting with help from: http://stackoverflow.com/questions/962802#962890
 
     for (var i = 0; i < 4; i++) rando[i] = i;
 
     function shuffle(array) {
+        // Create variables tmp, current, and top.  Top is the length of the passed array.
         var tmp, current, top = array.length;
+        
+        // Checks top exists.
         if (top)
             while (top--) {
                 current = Math.floor(Math.random() * (top + 1));
@@ -207,10 +229,12 @@ function run() {
         return array;
     }
 
+    // Redefine empty rando array as shuffled rando array.
     rando = shuffle(rando);
 
     console.log(rando);
 
+    // Assign numbers from rando to answers.
     var a1 = rando[0];
 
     var a2 = rando[1];
@@ -219,7 +243,10 @@ function run() {
 
     var a4 = rando[3];
 
+    // Question = questions object, index 4 of q+num array.
     $("#questionShow").html("<h2>Question: " + questions["q" + num][4] + "</h2");
+
+    // Answer = questions object, index random of q+num array.
     $("#answer1").html(questions["q" + num][a1]);
     $("#answer2").html(questions["q" + num][a2]);
     $("#answer3").html(questions["q" + num][a3]);
@@ -233,22 +260,28 @@ function decrement() {
     //  Decrease tickTock by one.
     tickTock--;
 
-    //  Show the number in the #timer tag.
+    //  Update the time left.
     $("#timerShow").html("<h2>" + tickTock + " seconds</h2>");
 
 
-    //  Once number hits zero...
+    //  If timer is at 0.
     if (tickTock === 0) {
 
-        //  ...run the stop function.
+        // Stop the timer.
         stop();
-        // nextQuestion();
 
-        //  Alert the user that time is up.
+        //  Hide gameShow.
         $("#gameShow").hide();
+
+        // Display time's up message with correct answer.
         $("#answerShow").html("<h2>Time's Up! The correct answer was \"" + questions["q" + num][0] + "\"</h2>");
+
+        // Delay next question for 1.5 seconds.
         setTimeout(function() {
+            // Blank out answerShow.
             $("#answerShow").html("");
+
+            // Move to next question.
             num++;
             run();
         }, 1500);
@@ -265,5 +298,5 @@ function stop() {
     tickTock = 15;
 }
 
-//  Execute the run function.
+//  And game is go.
 run();
